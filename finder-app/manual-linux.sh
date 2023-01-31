78c2e5682fc32d1e9241a2cca74f0a217483f326
Nzorro1978 <nzojihipolito@gmail.com>
2023-01-31 03:29:30 +0000

    before using crosstool-ng

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,12 +7,12 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
+KERNEL_VERSION=v5.4.50
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
-
+CROSS_COMPILE=arm-none-linux-gnueabihf-
+export CROSS_COMPILE=arm-none-linux-gnueabihf-
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -35,6 +35,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    #1 - build clean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} clean
+    #2 build defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+    # make -j 4 ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    #3 build vmlinuz
+    make -j 4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+else
+    cp -fr ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 fi
 
 echo "Adding the Image in outdir"
@@ -48,33 +59,73 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir "${OUTDIR}"/rootfs
+cd "${OUTDIR}"/rootfs
+mkdir bin dev etc home lib proc sbin sys tmp usr var
+mkdir usr/{bin,lib,sbin}
+mkdir -p var/log
 
+export CONFIG_PREFIX=${OUTDIR}/rootfs
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone https://github.com/mirror/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} clean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+    make -j 4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
+cd ${OUTDIR}/rootfs
+echo "Current Directory: ${PWD}"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+export SYSROOT=$(arm-none-linux-gnueabihf-gcc -print-sysroot)
+cd ${OUTDIR}/rootfs
+cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib
+cp -a ${SYSROOT}/lib64/ld-2.31.so lib
+cp -a ${SYSROOT}/lib64/libm.so.6 lib
+cp -a ${SYSROOT}/lib64/libm-2.31.so lib
+cp -a ${SYSROOT}/lib64/libresolv.so.2 lib
+cp -a ${SYSROOT}/lib64/libresolv-2.31.so lib
+cp -a ${SYSROOT}/lib64/libc.so.6 lib
+cp -a ${SYSROOT}/lib64/libc-2.31.so lib
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+
 
 # TODO: Clean and build the writer utility
+cd $FINDER_APP_DIR
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} clean
+make -j 4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} DESTDIR=${OUTDIR}/rootfs
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+sudo cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+sudo cp finder-test.sh ${OUTDIR}/rootfs/home/
+sudo cp writer ${OUTDIR}/rootfs/home/
+
+cd ${OUTDIR}/rootfs
 
 # TODO: Chown the root directory
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ..
+# gzip initramfs.cpio
+
