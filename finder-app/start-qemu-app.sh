4d88ec151cc4285f9704192843d7acf808a66a80
Nzorro1978 <nzojihipolito@gmail.com>
2023-02-02 17:18:10 +0000

    Attempt to Fix qemu-start script again

--- finder-app/start-qemu-app.sh
+++ finder-app/start-qemu-app.sh
@@ -29,7 +29,6 @@ echo "Booting the kernel"
 qemu-system-aarch64 \
         -m 256M \
         -M virt \
-        -cpu cortex-a53 \
         -nographic \
         -smp 1 \
         -kernel ${KERNEL_IMAGE} \
