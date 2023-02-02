4d2886422a34728911d75e0db94bc0339deebe4a
Nzoji Hipolito <71729152+nzorro1978@users.noreply.github.com>
2023-02-02 22:57:41 +0000

    Update start-qemu-app.sh

--- finder-app/start-qemu-app.sh
+++ finder-app/start-qemu-app.sh
@@ -11,7 +11,7 @@ if [ -z "${OUTDIR}" ]; then
     echo "No outdir specified, using ${OUTDIR}"
 fi
 
-KERNEL_IMAGE=${OUTDIR}/Image
+KERNEL_IMAGE=${OUTDIR}/zImage
 INITRD_IMAGE=${OUTDIR}/initramfs.cpio.gz
 
 if [ ! -e ${KERNEL_IMAGE} ]; then
@@ -27,8 +27,9 @@ fi
 echo "Booting the kernel"
 # See trick at https://superuser.com/a/1412150 to route serial port output to file
 qemu-system-aarch64 \
-        -m 256M \
+        -m 512M \
         -M virt \
+        -cpu cortex-a53 \
         -nographic \
         -smp 1 \
         -kernel ${KERNEL_IMAGE} \
